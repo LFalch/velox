6b4f42f069600648f80e021b7d0a10e2c40e63b9
LFalch <lucas@wasd.dk>
2016-01-23 15:20:54 +0100

    Use korome v0.9
    Make planets spawn when you click the mouse on the screen

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,49 @@
 #[macro_use]
 extern crate korome;
-extern crate glium;
 
 use korome::*;
 
 mod obj;
 use obj::*;
 
+trait Object: Draw + Update{}
+
+impl<T: Draw + Update> Object for T{}
+
 fn main() {
-    let title = format!("SPACE-SHOOTER (korome {})", korome::VERSION);
+    let graphics = Graphics::new("SPACE-SHOOTER", 1200, 900);
 
-    let draw = Draw::new(&title, 800, 600);
+    let planet = include_texture!(graphics, "planet.png").unwrap();
+    let player = include_texture!(graphics, "ship.png"  ).unwrap();
 
-    let planet = include_texture!(draw, "planet.png", 64, 64).unwrap();
-    let player = include_texture!(draw, "ship.png"  , 32, 32).unwrap();
+    let mut objs = Vec::<Box<Object>>::new();
+    objs.push(Box::new(Player::new(&player)));
 
-    let mut objs = Vec::new();
+    let mut gm = GameManager::new(graphics);
 
-    objs.push(Obj{
-        rot: 0.,
-        pos: Vector2(0., 0.),
-        vel: Vector2(0., 0.),
-        tex: &planet
-    });
-
-    objs.push(Obj{
-        rot: 0.,
-        pos: Vector2(0., 0.),
-        vel: Vector2(0., 0.),
-        tex: &player
-    });
-
-    let game = Game::with_shared(draw, objs, logic, render);
-
-    game.run_until_closed();
-}
-
-pub fn logic (objs: &mut Vec<Obj>, logic_args: LogicArgs){
-    {
-        let delta = logic_args.delta as f32;
-
-        let ref mut player = objs[1];
-
-        let mut acceleration = 0.;
-
-        is_down!{logic_args;
-            D, Right => {
-                player.rot -= 2. * delta
-            },
-            A, Left => {
-                player.rot += 2. * delta
-            },
-            S, Down => {
-                acceleration -= 3. * delta
-            },
-            W, Up => {
-                acceleration += 3. * delta
+    'main: while let Some((info, mut drawer)) = gm.next_frame(){
+        for ke in info.get_key_events(){
+            if let (false, VirtualKeyCode::Escape) = *ke{
+                break 'main
             }
         }
 
-        player.vel = player.vel + Vector2::<f32>::unit_vector(player.rot) * acceleration;
-    }
+        for me in info.get_mouse_events(){
+            if let (false, MouseButton::Left) = *me {
+                let obj = Obj{
+                    pos: info.mousepos.into(),
+                    .. Obj::new(&planet)
+                };
+                objs.push(Box::new(obj));
 
-    for obj in objs.iter_mut(){
-        obj.update()
-    }
-}
+                println!("Object added, new count: {}", objs.len());
+            }
+        }
 
-pub fn render(objs: &Vec<Obj>, mut render_args: RenderArgs){
-    render_args.draw_sprites(objs).unwrap();
+        drawer.clear(0., 0., 0.);
+        for obj in objs.iter_mut().rev(){
+            obj.update(&info);
+            obj.draw(&mut drawer).unwrap()
+        }
+    }
 }
