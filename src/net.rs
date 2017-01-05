e0881f29b61d405756b757188ea03c7a23bfaa1f
LFalch <lucas@wasd.dk>
2017-01-05 20:07:09 +0100

    Unwrap less in `net`

--- src/net.rs
+++ src/net.rs
@@ -29,6 +29,11 @@ pub enum ServerPacket {
     DisconnectAck
 }
 
+fn invalid_data_error<E>(e: E) -> Error
+where E: Into<Box<::std::error::Error + Send + Sync>>{
+    Error::new(::std::io::ErrorKind::InvalidData, e)
+}
+
 const BUFFER_SIZE: usize = 1024;
 
 pub struct ClientSocket(UdpSocket);
@@ -37,19 +42,20 @@ impl ClientSocket {
     pub fn new<S: ToSocketAddrs>(server: S) -> Self {
         let s = UdpSocket::bind("0.0.0.0:0").unwrap();
         s.connect(server).unwrap();
-        s.send(&encode(&ClientPacket::Connect, SizeLimit::Infinite).unwrap()).unwrap();
-        if let Ok(addr) = s.local_addr() {
+        let s = ClientSocket(s);
+        s.send(ClientPacket::Connect).unwrap();
+        if let Ok(addr) = s.0.local_addr() {
             println!("Bound to {}", addr);
         }
-        ClientSocket(s)
+        s
     }
     pub fn recv(&self) -> Result<ServerPacket, Error> {
         let mut buf = [0u8; BUFFER_SIZE];
         self.0.recv(&mut buf)
-            .map(|size| decode(&buf[..size]).unwrap())
+            .and_then(|size| decode(&buf[..size]).map_err(invalid_data_error))
     }
     pub fn send(&self, packet: ClientPacket) -> Result<usize, Error> {
-        let d = encode(&packet, SizeLimit::Infinite).unwrap();
+        let d = encode(&packet, SizeLimit::Infinite).map_err(invalid_data_error)?;
         self.0.send(&d)
     }
 }
@@ -60,24 +66,25 @@ impl ServerSocket {
     pub fn new<S: ToSocketAddrs>(bind_addr: S) -> Self {
         ServerSocket(UdpSocket::bind(bind_addr).unwrap())
     }
-    pub fn recv(&self) -> Option<(SocketAddr, ClientPacket)> {
+    pub fn recv(&self) -> Result<(SocketAddr, ClientPacket), Error> {
         let mut buf = [0u8; 20];
-        let ret = match self.0.recv_from(&mut buf) {
-            Ok((size, remote)) => decode(&buf[..size]).ok().map(|p| (remote, p)),
-            Err(_) => None,
-        };
-        ret
+        match self.0.recv_from(&mut buf) {
+            Ok((size, remote)) => {
+                decode(&buf[..size]).map(|p| (remote, p)).map_err(invalid_data_error)
+            }
+            Err(e) => Err(e),
+        }
     }
     pub fn send_all<'a, I: 'a>(&self, packet: ServerPacket, addrs: I) -> Result<(), Error>
     where I: IntoIterator<Item=&'a SocketAddr> {
-        let data = encode(&packet, SizeLimit::Infinite).unwrap();
+        let data = encode(&packet, SizeLimit::Infinite).map_err(invalid_data_error)?;
         for addr in addrs {
             self.0.send_to(&data, addr)?;
         }
         Ok(())
     }
     pub fn send(&self, packet: ServerPacket, addr: &SocketAddr) -> Result<usize, Error> {
-        let data = encode(&packet, SizeLimit::Infinite).unwrap();
+        let data = encode(&packet, SizeLimit::Infinite).map_err(invalid_data_error)?;
         self.0.send_to(&data, addr)
     }
 }
